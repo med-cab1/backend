fa48b733efb7d88f39feb2e10a22206213f1dc8e
mattorth <orth.mj@gmail.com>
2020-03-02 19:17:18 -0800

    update

--- knexfile.js
+++ knexfile.js
@@ -45,10 +45,7 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
       directory: './data/migrations'
     },
