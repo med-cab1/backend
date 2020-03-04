1bbaade59187f479084ce48ffb096bf10516381d
mattorth <orth.mj@gmail.com>
2020-03-03 21:10:04 -0800

    udpate

--- cannabis/cannabis-router.js
+++ cannabis/cannabis-router.js
@@ -69,10 +69,22 @@ router.post('/:id/recommendations', (req, res) => {
         });
 });
 
+// router.post('/prediction', (req, res) => {
+//     let info = req.body;
+
+//     axios.get(`https://unburied-medcab.herokuapp.com/prediction?disease=${info[0]}&effect1=${info[4]}&effect2=${info[5]}&effect3=${info[6]}&effect4=${info[7]}&effect5=${info[8]}&flavor1=${info[1]}&flavor2=${info[2]}&flavor3=${info[3]}`)
+//         .then(res => {
+//             res.status(201).json(res);
+//         })
+//         .catch(err => {
+//             res.status(404).json({ errorMessage: err, info: req.body})
+//         })
+// });
+
 router.post('/prediction', (req, res) => {
     let info = req.body;
 
-    axios.get(`https://unburied-medcab.herokuapp.com/prediction?disease=${info[0]}&effect1=${info[4]}&effect2=${info[5]}&effect3=${info[6]}&effect4=${info[7]}&effect5=${info[8]}&flavor1=${info[1]}&flavor2=${info[2]}&flavor3=${info[3]}`)
+    axios.post(`https://unburied-medcab.herokuapp.com/prediction`, info)
         .then(res => {
             res.status(201).json(res);
         })
