53abb91b85eb5bcd92f9bec4baacb23be94256ef
mattorth <orth.mj@gmail.com>
2020-03-04 18:27:55 -0800

    update

--- cannabis/cannabis-router.js
+++ cannabis/cannabis-router.js
@@ -78,7 +78,7 @@ router.post('/prediction', (req, res) => {
             res.status(201).json(res);
         })
         .catch(err => {
-            res.status(404).json({ errorMessage: err, info: req.body, request: `https://unburied-medcab.herokuapp.com/prediction?disease=${info[0]}&effect1=${info[4]}&effect2=${info[5]}&effect3=${info[6]}&effect4=${info[7]}&effect5=${info[8]}&flavor1=${info[1]}&flavor2=${info[2]}&flavor3=${info[3]}`})
+            res.status(404).json({ errorMessage: err, info: req.body, request: queryString })
         })
 });
 
